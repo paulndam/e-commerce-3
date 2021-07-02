9f96e5528780f68efa047a376d52cc351ebafef1
Paul Ndam <paulndam@Pauls-MacBook-Air-2.local>
2021-07-02 00:31:28 -0400

    created-react-app adding home page and sass folder and files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./Sass/App.scss";
+
+import HomePage from "./components/HomePage/HomePage.jsx";
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
