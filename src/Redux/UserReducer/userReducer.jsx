4ab72eb692c1f00e87ebdf0a2bde138ba6d7ac30
Paul Ndam <paulndam@Pauls-MacBook-Air-2.local>
2021-07-03 14:49:07 -0400

    updated app tp redirect user to home page once they sign in

--- src/Redux/UserReducer/userReducer.jsx
+++ src/Redux/UserReducer/userReducer.jsx
@@ -1,3 +1,5 @@
+import { userActionTypes } from "./userActionTypes";
+
 // a function that gets two props.
 // get a last state or the current state,then an action.
 // the action is obj that is a type that get a string value.
@@ -10,7 +12,7 @@ const INITIAL_STATE = {
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case "SET_CURRENT_USER":
+    case userActionTypes.SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload,
--- src/components/Directory/Directory.jsx
+++ src/components/Directory/Directory.jsx
@@ -10,25 +10,29 @@ class Directory extends Component {
       sections: [
         {
           title: "hats",
-          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          imageUrl:
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/a190fb98-650a-4584-bb0b-d13a3b4603d4/featherlight-womens-running-cap-127RF5.png",
           id: 1,
           linkUrl: "hats",
         },
         {
           title: "jackets",
-          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          imageUrl:
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/b9ede720-9662-4104-87b7-89e3b1209760/club-am%C3%A9rica-repel-academy-awf-mens-soccer-jacket-dtX3zm.png",
           id: 2,
           linkUrl: "shop/jackets",
         },
         {
           title: "sneakers",
-          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          imageUrl:
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/b12c27c7-aaee-450d-ae5e-0aa224ef3789/space-hippie-01-shoe-14Tq8r.png",
           id: 3,
           linkUrl: "shop/sneakers",
         },
         {
           title: "womens",
-          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          imageUrl:
+            "https://assets.adidas.com/images/w_766,h_766,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/ce53baba196e4aeaac57ac8601300c8d_9366/adicolor-tricolor-tank-dress.jpg",
           size: "large",
           id: 4,
           linkUrl: "shop/womens",
@@ -44,9 +48,9 @@ class Directory extends Component {
         {
           title: "mens",
           imageUrl:
-            "https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+            "https://static.zara.net/photos///2021/V/0/2/p/9240/409/310/2/w/527/9240409310_2_1_1.jpg?ts=1620662337878",
           size: "large",
-          id: 5,
+          id: 6,
           linkUrl: "shop/mens",
         },
       ],
