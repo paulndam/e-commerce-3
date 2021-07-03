117e38c825f660eff00f5da838e397c0623a83a9
Paul Ndam <paulndam@Pauls-MacBook-Air-2.local>
2021-07-02 22:37:48 -0400

    added header component and links to home page and shop page, next is to build sign-in & out page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import "./Sass/App.scss";
 
 import HomePage from "./pages/HomePage/HomePage.jsx";
 import ShopPage from "./pages/ShopPage/ShopPage";
+import Header from "./components/Header/Header.jsx";
 
 function App() {
   return (
     <div>
-      {/* <HomePage /> */}
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
