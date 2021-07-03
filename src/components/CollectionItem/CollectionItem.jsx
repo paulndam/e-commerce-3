54024a75924d11fd47895a9a0bf307d9cce0da62
Paul Ndam <paulndam@Pauls-MacBook-Air-2.local>
2021-07-03 15:56:27 -0400

    added styles for add to cart button in our shopping page

--- src/components/CollectionItem/CollectionItem.jsx
+++ src/components/CollectionItem/CollectionItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CustomButton from "../CustomButton/CustomButton";
 import "../../Sass/collection-item.styles.scss";
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
@@ -8,6 +9,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       <span className="name">{name}</span>
       <span className="price">{price}</span>
     </div>
+    <CustomButton>Add to Bag</CustomButton>
   </div>
 );
 
