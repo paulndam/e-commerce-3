429e0bc9811c57836d1431e4d3d05097825283bb
Paul Ndam <paulndam@Pauls-MacBook-Air-2.local>
2021-07-07 02:47:21 -0400

    added directoy reducer section , so that we can display each catergory by section

--- src/Redux/Reducer/rootReducer.js
+++ src/Redux/Reducer/rootReducer.js
@@ -4,6 +4,7 @@ import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import userReducer from "../UserReducer/userReducer";
 import cartReducer from "../CartReducer/cartReducer";
+import directoryReducer from "../Directory/directoryReducer";
 
 const persistConfig = {
   key: "root",
@@ -14,6 +15,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
